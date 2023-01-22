6d86eb0baadd93c703bb5bda843534f5724e452a
WhoReadThisWillDie <msadkoff@mail.ru>
2023-01-22 20:56:32 +0300

    вроде всё

--- app/src/main/java/com/example/homework_9/MySurfaceView.java
+++ app/src/main/java/com/example/homework_9/MySurfaceView.java
@@ -16,11 +16,13 @@ import java.util.Calendar;
 public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
     private SurfaceHolder surfaceHolder;
     private final DrawThread drawThread;
-
+    private MyCircle circle;
+    private Canvas canvas;
     public MySurfaceView(Context context) {
         super(context);
         getHolder().addCallback(this);
         drawThread = new DrawThread();
+        Log.d("RRR", "initialized");
     }
 
 
@@ -28,6 +30,8 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
     public void surfaceCreated(@NonNull SurfaceHolder surfaceHolder) {
         this.surfaceHolder = surfaceHolder;
         drawThread.start();
+        circle = new MyCircle(getWidth() / 2f, getHeight() / 2f);
+        Log.d("RRR", "created");
     }
 
     @Override
@@ -42,12 +46,13 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        circle.x1 = event.getX();
+        circle.y1 = event.getY();
         return super.onTouchEvent(event);
     }
 
     class DrawThread extends Thread {
-        Canvas canvas;
-        private boolean running;
+        private volatile boolean running = true;
 
         public void run() {
             while (running) {
@@ -55,32 +60,41 @@ public class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback
                     sleep(100);
                     canvas = surfaceHolder.lockCanvas();
                     canvas.drawColor(Color.BLUE);
+                    circle.draw(canvas);
+                    circle.update();
+                    surfaceHolder.unlockCanvasAndPost(canvas);
                 } catch (Exception e) {
                     Log.e("RRR", e.getMessage());
                 }
             }
         }
+    }
 
-        class MyCircle {
-            float x, y, radius;
-            Paint paint;
+    class MyCircle {
+        float x, y, radius, x1, y1;
 
-            public MyCircle(float x, float y, float radius, Paint paint) {
-                this.x = x;
-                this.y = y;
-                this.radius = 50;
-                this.paint = new Paint();
-                paint.setColor(Color.RED);
-            }
+        Paint paint;
 
-            void draw() {
-                canvas.drawCircle(x, y, radius, paint);
-            }
+        public MyCircle(float x, float y) {
+            this.x = x;
+            this.y = y;
+            this.radius = 50;
+            this.x1 = x;
+            this.y1 = y;
+            this.paint = new Paint();
+            paint.setColor(Color.RED);
+        }
 
-            void update(float dx, float dy) {
-                this.x += dx;
-                this.y += dy;
+        void draw(Canvas canvas) {
+            canvas.drawCircle(x, y, radius, paint);
+        }
+
+        void update() {
+            if (Math.abs((int)circle.x - (int)circle.x1) > 1 && Math.abs((int)circle.y - (int)circle.y1) > 1) {
+                x += (int) (x1 - x) / Math.sqrt(Math.pow(y1 - y, 2) + Math.pow(x1 - x, 2)) * 20;
+                y += (int) (y1 - y) / Math.sqrt(Math.pow(y1 - y, 2) + Math.pow(x1 - x, 2)) * 20;
             }
         }
     }
 }
+
